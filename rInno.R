cd04d6969999682b6f11c5aa1d8b35a0357eedf3
Christian Hermann <christian.hermann94@alice.de>
2023-07-30 23:45:00 +0200

    fixed desktop shortcut creation

--- rInno.R
+++ rInno.R
@@ -5,10 +5,9 @@ require(RInno)
 create_app(
   app_name = "Chanalyze",
   publisher = "Christian Hermann",
-  license_file = "About/License.txt",
-  app_icon  = "www/Chanalyze.ico",
+  license_file = "About\\License.txt",
   compression = "bzip",
-  info_after = "About/Contact.txt",
+  info_after = "About\\Contact.txt",
   pkgs = c(
     "plotly",
     "shinyBS",
@@ -47,7 +46,15 @@ create_app(
     "rvg",
     "officer"
   ),
-  user_browser = "electron"
-)
+  user_browser = "electron",
+  app_desc       = "Processing, evaluation and analysis of patch clamp data",
+  app_icon       = "www\\Chanalyze.ico",
+  prog_menu_icon = T,
+  desktop_icon   = T)
+
+#Change iss file after:
+#Under Icon, change commondesktop to userdesktop.
+
+
 
 compile_iss()
